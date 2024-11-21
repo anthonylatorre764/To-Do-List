86f0b1dbc71e73a43fbc2261be46dfa2495f9463
anthonylatorre764 <anthonylatorre764@gmail.com>
2024-11-21 00:45:45 -0500

    Added addNewTask function

--- script.js
+++ script.js
@@ -8,21 +8,45 @@ console.log("testing123")
 let newTask = ''
 let form = document.querySelector("form")
 
-
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     newTask = document.getElementById("inputField").value;
-    console.log(newTask)
+    addNewTask()
 });
 
+
+
 // 2. Add User Input to top of To-Do list
-// apendChild()
+let paragraphIndex = 0
+
+function addNewTask() {
+    // Get parent element
+    const toDoList = document.getElementById("toDoList");
+    
+    // Create child element
+    const listItem = document.createElement('div');
+    listItem.className = 'listItem';
+    
+    listItem.innerHTML = `
+        <input type="checkbox">
+        <p id="itemText${paragraphIndex}">${newTask}</p>
+    `;
+    
+    // Append child element to parent element
+    toDoList.appendChild(listItem);
+    paragraphIndex++;
+}
 
 
 
 // 3. Remove a specified task from list (minus button)
 // removeChild()
 
+function removeTask() {
+
+
+    paragraphIndex--
+}
 
 
 
